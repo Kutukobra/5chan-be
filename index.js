262c80462a29571b841926401f61de449c74cadf
Kutukobra <nadzhiff@gmail.com>
2025-05-05 11:48:23 +0700

    Rate limit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 15 minutes
+	windowMs: 15 * 60 * 1000, // 15 minutes
 	limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
