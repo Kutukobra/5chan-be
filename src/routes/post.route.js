4be0bc31812c1eb8974d5e61d5b0aca458d9019d
Kutukobra <nadzhiff@gmail.com>
2025-05-07 14:25:38 +0700

    gif support

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -16,7 +16,7 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
         folder: 'sbd',
-        allowed_formats: ['jpg', 'png', 'jpeg']
+        allowed_formats: ['jpg', 'png', 'jpeg', 'gif', 'svg']
     }
 });
 
